5843bbde9c306a43f8df1b641055d00855cce3c8
aja-silason <ananiasjaimeaugusto@gmail.com>
2025-04-05 11:38:05 +0100

    style: adiciona estilo do scroll

--- src/myportfolio/components/layouts/container.tsx
+++ src/myportfolio/components/layouts/container.tsx
@@ -13,23 +13,33 @@ export const Container = ({children}: props) => {
             
                 <div className="absolute inset-0 animate-waves bg-[url('/waves.svg')] bg-repeat-x bg-[length:200%_100%] opacity-20 z-0" />
                 
-                <div className="relative z-10 flex items-center justify-center h-full text-white">
-            
-                    <h1 className="text-4xl font-bold">Meu Portfólio Elegante</h1>
-            
-                    <p className="text-white">Olala</p>
-
-                    <div>
-                        <Header />
-                    </div>
-                    <div>
+                <div className="relative z-10 flex items-center justify-center h-full text-white overflow-hidden flex-col">
+                    <div className="border md:w-[960px] md:h-[720px] w-full p-[1em] overflow-hidden overflow-y-auto">
                         {children}
+                        <p className="border p-[1em]">p</p>
+                        <p className="border p-[1em]">p</p>
+                        <p className="border p-[1em]">p</p>
+                        <p className="border p-[1em]">p</p>
+                        <p className="border p-[1em]">p</p>
+                        <p className="border p-[1em]">p</p>
+                        <p className="border p-[1em]">p</p>
+                        <p className="border p-[1em]">p</p>
+                        <p className="border p-[1em]">p</p>
+                        <p className="border p-[1em]">p</p>
+                        <p className="border p-[1em]">p</p>
+                        <p className="border p-[1em]">p</p>
+                        <p className="border p-[1em]">p</p>
+                        <p className="border p-[1em]">p</p>
+                        <p className="border p-[1em]">p</p>
+                        <p className="border p-[1em]">p</p>
+                        <p className="border p-[1em]">p</p>
+                        <p className="border p-[1em]">p</p>
+                        <p className="border p-[1em]">p</p>
+                        <p className="border p-[1em]">p</p>
+                        <p className="border p-[1em]">p</p>
+                        <p className="border p-[1em]">p</p>
+                        <p className="border p-[1em]">p</p>
                     </div>
-                    <div>
-                        <Footer/>
-                    </div>
-
-
                 </div>
 
             </div>
