34e66090c21ae9697aac3c0c94b4e08a517cf6e2
aja-silason <ananiasjaimeaugusto@gmail.com>
2025-04-05 13:36:59 +0100

    feat: adicionando estreka

--- src/myportfolio/components/layouts/container.tsx
+++ src/myportfolio/components/layouts/container.tsx
@@ -8,8 +8,8 @@ export const Container = ({children}: props) => {
         <div className="bg-[#0b1225]">
 
             <div className="relative w-full h-screen overflow-hidden bg-[#0b1225]">
-            
                 <div className="absolute inset-0 animate-waves bg-[url('/waves.svg')] bg-repeat-x bg-[length:200%_100%] opacity-20 z-0" />
+                <div className="absolute bg-transparent inset-0 z-0 h-full stars"/>
                 
                 <div className="relative z-10 flex items-center justify-center h-full text-white overflow-hidden flex-col">
                     <div className="md:w-[960px] md:h-[720px] w-full p-[1em] overflow-hidden overflow-y-auto">
