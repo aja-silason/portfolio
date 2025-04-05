3f00ba5ca22097a09425a8d11356885efd437924
aja-silason <ananiasjaimeaugusto@gmail.com>
2025-04-05 11:45:34 +0100

    feat: adiciona dependencia axios e phosphor icon

--- src/myportfolio/components/layouts/container.tsx
+++ src/myportfolio/components/layouts/container.tsx
@@ -1,6 +1,4 @@
 import { ReactNode } from "react"
-import { Header } from "./navbar"
-import { Footer } from "./footer"
 
 type props = {
     children: ReactNode
@@ -14,31 +12,8 @@ export const Container = ({children}: props) => {
                 <div className="absolute inset-0 animate-waves bg-[url('/waves.svg')] bg-repeat-x bg-[length:200%_100%] opacity-20 z-0" />
                 
                 <div className="relative z-10 flex items-center justify-center h-full text-white overflow-hidden flex-col">
-                    <div className="border md:w-[960px] md:h-[720px] w-full p-[1em] overflow-hidden overflow-y-auto">
+                    <div className="md:w-[960px] md:h-[720px] w-full p-[1em] overflow-hidden overflow-y-auto">
                         {children}
-                        <p className="border p-[1em]">p</p>
-                        <p className="border p-[1em]">p</p>
-                        <p className="border p-[1em]">p</p>
-                        <p className="border p-[1em]">p</p>
-                        <p className="border p-[1em]">p</p>
-                        <p className="border p-[1em]">p</p>
-                        <p className="border p-[1em]">p</p>
-                        <p className="border p-[1em]">p</p>
-                        <p className="border p-[1em]">p</p>
-                        <p className="border p-[1em]">p</p>
-                        <p className="border p-[1em]">p</p>
-                        <p className="border p-[1em]">p</p>
-                        <p className="border p-[1em]">p</p>
-                        <p className="border p-[1em]">p</p>
-                        <p className="border p-[1em]">p</p>
-                        <p className="border p-[1em]">p</p>
-                        <p className="border p-[1em]">p</p>
-                        <p className="border p-[1em]">p</p>
-                        <p className="border p-[1em]">p</p>
-                        <p className="border p-[1em]">p</p>
-                        <p className="border p-[1em]">p</p>
-                        <p className="border p-[1em]">p</p>
-                        <p className="border p-[1em]">p</p>
                     </div>
                 </div>
 
--- src/myportfolio/home.tsx
+++ src/myportfolio/home.tsx
@@ -1,9 +1,10 @@
+import { ProfileCard } from "./components/cards/profile.card";
 import { Container } from "./components/layouts/container";
 
 export function Home () {
     return (
         <Container>
-            <p> Home</p>
+            <ProfileCard />
         </Container>
     )
 }
